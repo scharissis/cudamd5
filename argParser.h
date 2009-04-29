088ba2e818c24da2370d78395603c1d36de7d3d2
peterwong86 <peterwong86@39bc5d14-33bc-11de-8e0c-53a27eea117e>
2009-04-29 09:12:08 +0000

    test

---
+++ argParser.h
@@ -0,0 +1,12 @@
+/*
+ *  argParser.h
+ *  CUDA Project
+ *
+ *  Created by Peter Wong on 29/04/09.
+ *  Copyright 2009 University of New South Wales. All rights reserved.
+ *
+ */
+
+
+
+int parseArg(int argc, char* argv[]);
