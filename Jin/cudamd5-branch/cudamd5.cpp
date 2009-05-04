4aa40229ca5608c933d9b1abd43e169d6811f05b
jl.listens <jl.listens@39bc5d14-33bc-11de-8e0c-53a27eea117e>
2009-05-04 04:51:03 +0000

    Added lots of testing tools under experiments/. Branched off a copy of the code and cleaned it up some more. Added some CUDA code to the branch (deviceQuery and squaring an array).

---
+++ Jin/cudamd5-branch/cudamd5.cpp
@@ -0,0 +1,78 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include "deviceQuery.h"
+#include "md5test.cpp"
+#include "Permutator.h"
+#include "arrayTest.h"
+
+using namespace std;
+
+//static void MDTimeTrial_CPU ();
+static void MDString (char *inString);
+
+int main(int argc, char *argv[]) {
+  bool isVerbose = false;
+  int minLen;
+  int maxLen;
+  string message;
+  
+	// Declare the supported options.
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,?", "Produces this help message")
+    ("query,q", "Checks system for CUDA compatibility")
+    ("verbose,v", "Turns on verbose mode")
+    ("target,t", po::value<string>(), "Target digest")
+    ("charset,c", po::value<string>(), "Message character set")
+    ("min", po::value<int>()->default_value(1), "Minimum message length")
+    ("max", po::value<int>()->default_value(3), "Maximum message length")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);    
+
+  if (vm.count("help")) {
+    cout << desc << endl;
+  }
+  else if (vm.count("query")) {
+    deviceQuery();
+  }
+  else if (vm.count("target") && vm.count("charset")) {    
+    isVerbose = vm.count("verbose");
+    minLen = vm["min"].as<int>();
+    maxLen = vm["max"].as<int>();
+    
+    if (isVerbose) {
+      cout << "Target digest: " << vm["target"].as<string>() << endl;
+      cout << "Message character set: " << vm["charset"].as<string>() << endl;
+      cout << "Minimum message length: " <<  minLen << endl;
+      cout << "Maximum message length: " <<  maxLen << endl;
+    }
+    
+    string charset(vm["charset"].as<string>());
+    Permutator p(charset);  
+    
+    string message(minLen, p.first);
+    string end(maxLen + 1, p.first);
+    
+    do {
+      MDString(message.c_str());
+      message = p.permutate(message);
+    } while (message != end);
+  }
+  else {
+    cout << desc << endl;
+  }
+  
+  return EXIT_SUCCESS;
+}
+
+
---
+++ Jin/cudamd5-branch/md5test.cpp
@@ -0,0 +1,195 @@
+/*
+ **********************************************************************
+ ** Copyright (C) 1990, RSA Data Security, Inc. All rights reserved. **
+ **                                                                  **
+ ** RSA Data Security, Inc. makes no representations concerning      **
+ ** either the merchantability of this software or the suitability   **
+ ** of this software for any particular purpose.  It is provided "as **
+ ** is" without express or implied warranty of any kind.             **
+ **                                                                  **
+ ** These notices must be retained in any copies of any part of this **
+ ** documentation and/or software.                                   **
+ **********************************************************************
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <string>
+#include "md5.h"
+
+/* Prints message digest buffer in mdContext as 32 hexadecimal digits.
+   Order is from low-order byte to high-order byte of digest.
+   Each byte is printed with high-order hexadecimal digit first.
+ */
+static void MDPrint (MD5_CTX *mdContext)
+{
+  int i;
+
+  for (i = 0; i < 16; i++)
+    printf ("%02x", mdContext->digest[i]);
+}
+
+/* size of test block */
+#define TEST_BLOCK_SIZE 1000
+
+/* number of blocks to process */
+#define TEST_BLOCKS 10000
+
+/* number of test bytes = TEST_BLOCK_SIZE * TEST_BLOCKS */
+static long TEST_BYTES = (long)TEST_BLOCK_SIZE * (long)TEST_BLOCKS;
+
+/* A time trial routine, to measure the speed of MD5.
+   Measures wall time required to digest TEST_BLOCKS * TEST_BLOCK_SIZE
+   characters.
+ */
+static void MDTimeTrial ()
+{
+  MD5_CTX mdContext;
+  time_t endTime, startTime;
+  unsigned char data[TEST_BLOCK_SIZE];
+  unsigned int i;
+
+  /* initialize test data */
+  for (i = 0; i < TEST_BLOCK_SIZE; i++)
+    data[i] = (unsigned char)(i & 0xFF);
+
+  /* start timer */
+  printf ("MD5 time trial. Processing %ld characters...\n", TEST_BYTES);
+  time (&startTime);
+
+  /* digest data in TEST_BLOCK_SIZE byte blocks */
+  MD5Init (&mdContext);
+  for (i = TEST_BLOCKS; i > 0; i--)
+    MD5Update (&mdContext, data, TEST_BLOCK_SIZE);
+  MD5Final (&mdContext);
+
+  /* stop timer, get time difference */
+  time (&endTime);
+  long timeTaken = (long)(endTime-startTime);
+  MDPrint (&mdContext);
+  printf (" is digest of test input.\n");
+  printf
+    ("Seconds to process test input: %ld\n", timeTaken);
+  if (timeTaken != 0){
+    printf
+      ("Characters processed per second: %ld\n",
+       TEST_BYTES/timeTaken);
+  } else {
+    printf("Time taken = 0.00 seconds!?\n");
+  }
+}
+
+
+
+/* Computes the message digest for string inString.
+   Prints out message digest, a space, the string (in quotes) and a
+   carriage return.
+ */
+static void MDString (const char *inString)
+{
+  MD5_CTX mdContext;
+  unsigned int len = strlen (inString);
+
+  MD5Init (&mdContext);
+  unsigned char *uinString = (unsigned char*)inString;
+  MD5Update (&mdContext, uinString, len);
+  MD5Final (&mdContext);
+  //DEBUG
+  //MDPrint (&mdContext);
+  //printf (" \"%s\"\n\n", inString);
+}
+
+/* Computes the message digest for a specified file.
+   Prints out message digest, a space, the file name, and a carriage
+   return.
+ */
+static void MDFile (char *filename)
+{
+  FILE *inFile = fopen (filename, "rb");
+  MD5_CTX mdContext;
+  int bytes;
+  unsigned char data[1024];
+
+  if (inFile == NULL) {
+    printf ("%s can't be opened.\n", filename);
+    return;
+  }
+
+  MD5Init (&mdContext);
+  while ((bytes = fread (data, 1, 1024, inFile)) != 0)
+    MD5Update (&mdContext, data, bytes);
+  MD5Final (&mdContext);
+  MDPrint (&mdContext);
+  printf (" %s\n", filename);
+  fclose (inFile);
+}
+
+/* Writes the message digest of the data from stdin onto stdout,
+   followed by a carriage return.
+ */
+static void MDFilter ()
+{
+  MD5_CTX mdContext;
+  int bytes;
+  unsigned char data[16];
+
+  MD5Init (&mdContext);
+  while ((bytes = fread (data, 1, 16, stdin)) != 0)
+    MD5Update (&mdContext, data, bytes);
+  MD5Final (&mdContext);
+  MDPrint (&mdContext);
+  printf ("\n");
+}
+
+// New time trial
+static void MDTimeTrial_CPU ()
+{
+  MD5_CTX mdContext;
+  time_t endTime, startTime;
+  std::string filename = "/usr/share/dict/words";
+	
+  FILE *inFile = fopen (filename.c_str(), "rb");
+  int bytes,n;
+  char word [42];
+  unsigned char data[1024];
+
+  if (inFile == NULL) {
+    printf ("%s can't be opened.\n", filename.c_str());
+    return;
+  }  
+  n = 0;
+  while (fscanf(inFile, "%s", word) != EOF){
+    //printf("%s\n", word);
+    MDString(word);
+    n++;
+  }
+  printf("%i words digested.\n", n);
+
+}
+
+
+/* Runs a standard suite of test data.
+ */
+
+static void MDTestSuite ()
+{
+/*
+  printf ("MD5 test suite results:\n\n");
+  MDString ("");
+  MDString ("a");
+  MDString ("abc");
+  MDString ("message digest");
+  MDString ("abcdefghijklmnopqrstuvwxyz");
+  MDString
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+  MDString
+    ("1234567890123456789012345678901234567890\
+1234567890123456789012345678901234567890");
+  // Contents of file foo are "abc" 
+  MDFile ("foo");
+*/
+}
+
+
