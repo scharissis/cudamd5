aa93eb6c2c982cd05b6e655f9e4d495d7ad25fd7
Stefano.Zeus <Stefano.Zeus@39bc5d14-33bc-11de-8e0c-53a27eea117e>
2009-05-30 13:31:50 +0000

    Hashing works!

--- gpuMD5.cu
+++ gpuMD5.cu
@@ -1,8 +1,8 @@
 #include "gpuMD5.h"
 #include "cutil.h"
 
-__device__ UINT F(UINT& x, UINT& y, UINT& z);
-__device__ UINT G(UINT& x, UINT& y, UINT& z);
+__device__ UINT F(UINT x, UINT y, UINT z);
+__device__ UINT G(UINT x, UINT y, UINT z);
 __device__ UINT H(UINT& x, UINT& y, UINT& z);
 __device__ UINT I(UINT& x, UINT& y, UINT& z);
 
@@ -13,11 +13,11 @@ __device__ void GG(UINT& a, UINT& b, UINT& c, UINT& d, UINT& x, UINT s, UINT ac)
 __device__ void HH(UINT& a, UINT& b, UINT& c, UINT& d, UINT& x, UINT s, UINT ac);
 __device__ void II(UINT& a, UINT& b, UINT& c, UINT& d, UINT& x, UINT s, UINT ac);
 
-/* F, G and H are basic MD5 functions: selection, majority, parity */
-__device__ UINT F(UINT& x, UINT& y, UINT& z){
+// F, G and H are basic MD5 functions: selection, majority, parity 
+__device__ UINT F(UINT x, UINT y, UINT z){
   return ((x & y) | (~x & z));
 }
-__device__ UINT G(UINT& x, UINT& y, UINT& z){
+__device__ UINT G(UINT x, UINT y, UINT z){
   return ((x & z) | (y & ~z));
 }
 __device__ UINT H(UINT& x, UINT& y, UINT& z){
@@ -27,13 +27,13 @@ __device__ UINT I(UINT& x, UINT& y, UINT& z){
   return (y ^ (x | ~z));
 }
 
-/* ROTATE_LEFT rotates x left n bits */
+// ROTATE_LEFT rotates x left n bits 
 __device__ UINT ROTATE_LEFT(UINT& x, UINT& n){
   return ((x << n) | (x >> (32 - n)));
 }
 
-/* FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4 */
-/* Rotation is separate from addition to prevent recomputation */__device__ void FF(UINT& a, UINT& b, UINT& c, UINT& d, UINT& x, UINT s, UINT ac){
+// FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4
+// Rotation is separate from addition to prevent recomputation __device__ void FF(UINT& a, UINT& b, UINT& c, UINT& d, UINT& x, UINT s, UINT ac){
   a += F(b, c, d) + x + ac; 
   a =  ROTATE_LEFT(a, s); 
   a += b;
@@ -195,15 +195,78 @@ __global__ void md5Hash(UCHAR** messages, int* msgLengths, uint4* digests) {
   FF ( d, a, b, c, in[13], S12, 4254626195); /* 14 */
   FF ( c, d, a, b, in[14], S13, 2792965006); /* 15 */
   FF ( b, c, d, a, in[15], S14, 1236535329); /* 16 */
-
+  
   /* Round 2 */
 #define S21 5
 #define S22 9
 #define S23 14
 #define S24 20
-//  GG ( a, b, c, d, in[ 1], S21, 4129170786); /* 17 */
-  
+  GG ( a, b, c, d, in[ 1], S21, 4129170786); /* 17 */
+  GG ( d, a, b, c, in[ 6], S22, 3225465664); /* 18 */
+  GG ( c, d, a, b, in[11], S23,  643717713); /* 19 */
+  GG ( b, c, d, a, in[ 0], S24, 3921069994); /* 20 */
+  GG ( a, b, c, d, in[ 5], S21, 3593408605); /* 21 */
+  GG ( d, a, b, c, in[10], S22,   38016083); /* 22 */
+  GG ( c, d, a, b, in[15], S23, 3634488961); /* 23 */
+  GG ( b, c, d, a, in[ 4], S24, 3889429448); /* 24 */
+  GG ( a, b, c, d, in[ 9], S21,  568446438); /* 25 */
+  GG ( d, a, b, c, in[14], S22, 3275163606); /* 26 */
+  GG ( c, d, a, b, in[ 3], S23, 4107603335); /* 27 */
+  GG ( b, c, d, a, in[ 8], S24, 1163531501); /* 28 */
+  GG ( a, b, c, d, in[13], S21, 2850285829); /* 29 */
+  GG ( d, a, b, c, in[ 2], S22, 4243563512); /* 30 */
+  GG ( c, d, a, b, in[ 7], S23, 1735328473); /* 31 */
+  GG ( b, c, d, a, in[12], S24, 2368359562); /* 32 */
 
+  /* Round 3 */
+#define S31 4
+#define S32 11
+#define S33 16
+#define S34 23
+  HH ( a, b, c, d, in[ 5], S31, 4294588738); /* 33 */
+  HH ( d, a, b, c, in[ 8], S32, 2272392833); /* 34 */
+  HH ( c, d, a, b, in[11], S33, 1839030562); /* 35 */
+  HH ( b, c, d, a, in[14], S34, 4259657740); /* 36 */
+  HH ( a, b, c, d, in[ 1], S31, 2763975236); /* 37 */
+  HH ( d, a, b, c, in[ 4], S32, 1272893353); /* 38 */
+  HH ( c, d, a, b, in[ 7], S33, 4139469664); /* 39 */
+  HH ( b, c, d, a, in[10], S34, 3200236656); /* 40 */
+  HH ( a, b, c, d, in[13], S31,  681279174); /* 41 */
+  HH ( d, a, b, c, in[ 0], S32, 3936430074); /* 42 */
+  HH ( c, d, a, b, in[ 3], S33, 3572445317); /* 43 */
+  HH ( b, c, d, a, in[ 6], S34,   76029189); /* 44 */
+  HH ( a, b, c, d, in[ 9], S31, 3654602809); /* 45 */
+  HH ( d, a, b, c, in[12], S32, 3873151461); /* 46 */
+  HH ( c, d, a, b, in[15], S33,  530742520); /* 47 */
+  HH ( b, c, d, a, in[ 2], S34, 3299628645); /* 48 */
+  
+    /* Round 4 */
+#define S41 6
+#define S42 10
+#define S43 15
+#define S44 21
+  II ( a, b, c, d, in[ 0], S41, 4096336452); /* 49 */
+  II ( d, a, b, c, in[ 7], S42, 1126891415); /* 50 */
+  II ( c, d, a, b, in[14], S43, 2878612391); /* 51 */
+  II ( b, c, d, a, in[ 5], S44, 4237533241); /* 52 */
+  II ( a, b, c, d, in[12], S41, 1700485571); /* 53 */
+  II ( d, a, b, c, in[ 3], S42, 2399980690); /* 54 */
+  II ( c, d, a, b, in[10], S43, 4293915773); /* 55 */
+  II ( b, c, d, a, in[ 1], S44, 2240044497); /* 56 */
+  II ( a, b, c, d, in[ 8], S41, 1873313359); /* 57 */
+  II ( d, a, b, c, in[15], S42, 4264355552); /* 58 */
+  II ( c, d, a, b, in[ 6], S43, 2734768916); /* 59 */
+  II ( b, c, d, a, in[13], S44, 1309151649); /* 60 */
+  II ( a, b, c, d, in[ 4], S41, 4149444226); /* 61 */
+  II ( d, a, b, c, in[11], S42, 3174756917); /* 62 */
+  II ( c, d, a, b, in[ 2], S43,  718787259); /* 63 */
+  II ( b, c, d, a, in[ 9], S44, 3951481745); /* 64 */
+  
+  a += h0;
+  b += h1;
+  c += h2;
+  d += h3;
+  
   digests[idx] = make_uint4(a, b, c, d);
 }
 
